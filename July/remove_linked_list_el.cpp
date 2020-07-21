fea119d8a18fc00ea6d6bae87a52768bcbe1e1ff
pulkitd2699 <pulkitd2699@gmail.com>
2020-07-21 10:16:20 +0530

    20 added

---
+++ July/remove_linked_list_el.cpp
@@ -0,0 +1,27 @@
+// Remove all elements from a linked list of integers that have value val.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL) return head;z
+        ListNode *p = head;
+        while(p->next != NULL){
+            if(p->next->val == val){
+                p->next = p->next->next;
+            }
+            else p = p->next;
+        }
+        if(head->val == val) head = head->next;
+        return head;
+    }
+};
